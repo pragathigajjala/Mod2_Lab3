9c23d5bf23dfbff5585149a37efe483668426fbd
pragathigajjala <46912681+pragathigajjala@users.noreply.github.com>
2019-02-06 22:09:06 -0800

    Add project files.

---
+++ Mod2_Lab3/Program.cs
@@ -0,0 +1,30 @@
+﻿//This Program displays prime numbers up to 10
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int outer;
+            int inner;
+
+            for (outer=2; outer<10; outer++)
+            {
+                for (inner = 2; inner <= (outer / inner); inner++)
+                    if ((outer % inner) == 0 )break;//if factor found, not prime
+                    if (inner > (outer / inner))
+                    Console.WriteLine("{0} is prime", outer);
+
+            }
+
+
+        }  
+    }
+}
